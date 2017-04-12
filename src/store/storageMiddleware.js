a927544a10c64a2f3883036fff26325df10adb29
Krysto <quy.p.hoang@gmail.com>
2017-04-12 23:33:20 +0700

    delayed storing data

--- src/store/storageMiddleware.js
+++ src/store/storageMiddleware.js
@@ -1,16 +1,33 @@
 import update from 'immutability-helper';
 
-export default store => next => action => {
-  const currentState = store.getState();
-  const currentTodoList = currentState.annotations;
+const timeout = 1000; // ms
+let timeoutRef = null;
+let annotations = undefined;
 
+const storeData = () => {
+  localStorage.setItem('annotations', JSON.stringify(annotations))
+  timeoutRef = null
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('Persisted annotations to localStorage', annotations);
+  }
+}
+
+export default store => next => action => {
   const result = next(action);
 
   const newState = store.getState();
-  const newTodoList = newState.annotations;
+  const newAnnotations = newState.annotations;
 
-  if (newTodoList !== currentTodoList) {
-    localStorage.setItem('annotations', JSON.stringify(newTodoList));
+  if (newAnnotations !== annotations) {
+    // annotations === undefined is on the very first action
+    // newAnnotations is the initial state
+    // just initialize annotations by assigning
+    const isInitial = annotations === undefined
+    annotations = newAnnotations;
+
+    if (!isInitial && !timeoutRef) {
+      timeoutRef = setTimeout(storeData, timeout)
+    }
   }
 
   return result;
